e39586133cfe9689de020173f49ba9743cc5f8dc
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-13 14:51:45 -0300

    Login com states
    Adição dos componentes da tela de login básica, bem como os states de cada campo

--- curso-react/src/app/pages/login/Login.tsx
+++ curso-react/src/app/pages/login/Login.tsx
@@ -1,14 +1,27 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () =>{
-    const history = useNavigate();
-    const handleClick = () =>{
-        history("/pagina-inicial");
+    //estados para armazenar os inputs
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEntrar = () =>{
+
     }
+
     return (
         <div>
-            Login
-            <button onClick={handleClick}>Tela inicial</button>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <button type="button" onClick={handleEntrar}>Entrar</button>
+            </form>
         </div>
     );
 }
