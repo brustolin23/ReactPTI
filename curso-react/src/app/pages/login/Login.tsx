c7db9e231cb8e0cd1199e08259940efb786cc836
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-14 15:08:36 -0300

    useRef básico
    Focando o próximo campo quando o "enter " for pressionado

--- curso-react/src/app/pages/login/Login.tsx
+++ curso-react/src/app/pages/login/Login.tsx
@@ -1,6 +1,8 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () =>{
+    const inputEmailRef = useRef<HTMLInputElement>(null);
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
     //estados para armazenar os inputs
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -11,6 +13,9 @@ export const Login = () =>{
     const handleEntrar = useCallback(() =>{
         console.log("Entrou");
         console.log(email);
+        if (inputEmailRef.current!=null) {
+            inputEmailRef.current.focus();
+        }
     },[email]);
 
     const emailLength = useMemo(() =>{
@@ -23,11 +28,11 @@ export const Login = () =>{
                 <p>Quantidade de caracteres de email é {emailLength}</p>
                 <label>
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input value={email} onChange={e => setEmail(e.target.value)} ref={inputEmailRef} onKeyDown={e => e.key=="Enter" ? inputPasswordRef.current?.focus() : undefined}/>
                 </label>
                 <label>
                     <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} ref={inputPasswordRef}/>
                 </label>
                 <button type="button" onClick={handleEntrar}>Entrar</button>
             </form>
