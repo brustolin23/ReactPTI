9e30dd6abc9d4168878a89e151f28d674157832e
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-22 15:43:33 -0300

    Configurando o novo React router dom

--- curso-materialui/src/App.tsx
+++ curso-materialui/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      Olá mundo
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   );
 }
