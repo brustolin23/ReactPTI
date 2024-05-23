91b103f32086cc37e0af8c49dda1c15b47240fbf
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-23 15:09:44 -0300

    Troca de temas

--- curso-materialui/src/routes/index.tsx
+++ curso-materialui/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate} from "react-router-dom";
+import { useAppThemContext } from "../shared/contexts/ThemeContext";
 
 
 export const AppRoutes = () => {
+    const {toggleTheme} = useAppThemContext();
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="secondary">Hello World</Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="secondary" onClick={toggleTheme}>Hello World</Button>}/>
             <Route path="*" element={<Navigate to={"/pagina-inicial"}/>}/>
         </Routes>
     );
---
+++ curso-materialui/src/shared/themes/Dark.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { blue, purple } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary:{
+            main: blue[700],
+            dark: blue[900],
+            light: blue[400],
+            contrastText: '#ffffff'
+        },
+        secondary: {
+            main: purple[700],
+            dark: purple[900],
+            light: purple[400],
+            contrastText: '#ffffff'
+        },
+        background: {
+            default: '#303030',
+            paper: '#202020'
+        }
+    }
+});
