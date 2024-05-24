02a4d1c2077af18ab0ffc4f5f56820ae4b0dae58
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-24 14:06:53 -0300

    Revert "Menu e remoção do eslint"
    This reverts commit 095b7f7db9d90064beff464bb954d7a8d9d2bc69.

--- curso-materialui/src/App.tsx
+++ curso-materialui/src/App.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
-import { MenuLateral } from "./shared/components";
 
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <MenuLateral>
-          <AppRoutes/>
-        </MenuLateral>
+        <AppRoutes/>
       </BrowserRouter>
     </AppThemeProvider>
   );
--- curso-materialui/src/shared/components/index.ts
+++ curso-materialui/src/shared/components/index.ts
@@ -1,1 +1,1 @@
-export * from './menuLateral/MenuLateral';
+export {};
