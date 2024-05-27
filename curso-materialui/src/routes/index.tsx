f0ce2b890d1dc9d68f65bee0238dd50d3e62a56e
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-05-27 15:54:18 -0300

    Responsividade do Menu
    Adicionada a responsividade do menu para telas pequenas

--- curso-materialui/src/routes/index.tsx
+++ curso-materialui/src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate} from "react-router-dom";
-import { useAppThemContext } from "../shared/contexts/ThemeContext";
+import { useDrawerContext } from "../shared/contexts";
 
 
 export const AppRoutes = () => {
-    const {toggleTheme} = useAppThemContext();
+    const {toggleDrawerOpen} = useDrawerContext();
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="secondary" onClick={toggleTheme}>Hello World</Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="secondary" onClick={toggleDrawerOpen}>Hello World</Button>}/>
             <Route path="*" element={<Navigate to={"/pagina-inicial"}/>}/>
         </Routes>
     );
