b2abb581d74743f85e4bdcb8d1f0092346ce853b
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-06-04 14:02:13 -0300

    Configuração do Axios

---
+++ curso-materialui/src/shared/services/api/axios-config/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { errorInterceptor, responseInterceptor } from "./interceptors";
+
+const Api = axios.create({
+    baseURL: "http://localhost:3333"
+})
+
+Api.interceptors.response.use((response)=>responseInterceptor(response), (error)=>errorInterceptor(error));
