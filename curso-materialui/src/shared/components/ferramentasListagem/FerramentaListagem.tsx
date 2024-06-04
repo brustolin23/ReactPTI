48fd22eace3d03c8e61695915991ad4432053a7e
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-06-04 14:24:54 -0300

    Configuração das variáveis de ambiente

--- curso-materialui/src/shared/components/ferramentasListagem/FerramentaListagem.tsx
+++ curso-materialui/src/shared/components/ferramentasListagem/FerramentaListagem.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+import { Environment } from "../../environment";
 
 interface IBarraFerramentasProps{
     textoBusca?: string,
@@ -17,7 +18,7 @@ export const FerramentaListagem: React.FC<IBarraFerramentasProps> = ({
     return(
         <Box gap={1} marginX={1} padding={1} paddingX={1} display="flex" alignItems="center" height={theme.spacing(5)} component={Paper}>
             {mostrarInputBusca &&(
-                <TextField value={textoBusca} onChange={(e) => mudarTextoBusca?.(e.target.value)} size="small" placeholder="Pesquisar..."></TextField>
+                <TextField value={textoBusca} onChange={(e) => mudarTextoBusca?.(e.target.value)} size="small" placeholder={Environment.INPUT_DE_BUSCA}></TextField>
             )}
             {mostrarBotao &&(
                 <Box flex={1} display="flex" justifyContent="end"><Button variant="contained" color="primary" disableElevation endIcon={<Icon>add</Icon>}>{textoBotao}</Button></Box>
--- curso-materialui/src/shared/environment/index.ts
+++ curso-materialui/src/shared/environment/index.ts
@@ -1,1 +1,18 @@
-export {};
+export const Environment = {
+    /**
+     * define a quantidade padrão de linhas carregadas nas listagens
+     */
+    LIMITE_DE_LINHAS:10,
+    /**
+     * Placeholder de inputs
+     */
+    INPUT_DE_BUSCA:"Pesquisar",
+    /**
+     * Mensagem ecibida quando não há itens na lista
+     */
+    LISTAGEM_VAZIA:"Nenhum registro encontrado",
+    /**
+     * URL base de consulta de dados
+     */
+    URL_BASE:"http://localhost:3333"
+};
