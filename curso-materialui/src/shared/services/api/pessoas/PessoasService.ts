b1d9b2540ccb75761b5de6a5903d6c6c78c2b3ea
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-06-04 15:43:39 -0300

    API Pessoa - parte 2

--- curso-materialui/src/shared/services/api/pessoas/PessoasService.ts
+++ curso-materialui/src/shared/services/api/pessoas/PessoasService.ts
@@ -36,17 +36,49 @@ const getAll = async(page:1, filter:''): Promise<TPessoasComTotalCount | Error> 
         return new Error((error as {message:string}).message||'Erro ao conectar')
     }
 }
-const getById = async(): Promise<any> =>{
-
+const getById = async(id:number): Promise<IDetalhePessoa | Error> =>{
+    try {
+        const urlRelativa = `/pessoas/${id}`
+        const{data}= await Api.get(urlRelativa)
+        if (data) {
+            return data
+        }
+        return new Error('Erro ao buscar os dados do usuário')
+    } catch (error) {
+        console.error(error)
+        return new Error((error as {message:string}).message||'Erro ao conectar')
+    }
 }
-const create = async(): Promise<any> =>{
-
+const create = async(dados:Omit<IDetalhePessoa, 'id'>): Promise<number | Error> =>{
+    try {
+        const urlRelativa = `/pessoas`
+        const{data}= await Api.post<IDetalhePessoa>(urlRelativa,dados)
+        if (data) {
+            return data.id
+        }
+        return new Error('Erro ao criar usuário')
+    } catch (error) {
+        console.error(error)
+        return new Error((error as {message:string}).message||'Erro ao conectar')
+    }
 }
-const deleteById = async(): Promise<any> =>{
-
+const deleteById = async(id:number): Promise<void | Error> =>{
+    try {
+        const urlRelativa = `/pessoas/${id}`
+        await Api.delete(urlRelativa)
+    } catch (error) {
+        console.error(error)
+        return new Error((error as {message:string}).message||'Erro ao conectar')
+    }
 }
-const updateById = async(): Promise<any> =>{
-
+const updateById = async(id:number, dados:IDetalhePessoa): Promise<void | Error> =>{
+    try {
+        const urlRelativa = `/pessoas/${id}`
+        await Api.put(urlRelativa, dados)
+    } catch (error) {
+        console.error(error)
+        return new Error((error as {message:string}).message||'Erro ao conectar')
+    }
 }
 
 export const PessoasService = {
